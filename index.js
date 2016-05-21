85266b7e2d08624043f8842a41f5454abcf05d87
sunnylost <sunnylost@gmail.com>
2016-05-21 11:13:35 +0800

    update electron to 1.1.1

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 'use strict';
 
-const electron      = require( 'electron' );
-const app           = electron.app
-const BrowserWindow = electron.BrowserWindow
+const electron      = require( 'electron' ),
+      app           = electron.app,
+      BrowserWindow = electron.BrowserWindow
 
 var mainWindow = null
 
-app.on( 'window-all-closed', function () {
-    if ( process.platform != 'darwin' ) {
-        app.quit()
-    }
+app.on( 'window-all-closed', () => {
+    app.quit()
 } )
 
 app.on( 'ready', function () {
     mainWindow = new BrowserWindow( {
-        width  : 800,
-        height : 600
+        width : 800,
+        height: 600
+    } )
+
+    mainWindow.on( 'closed', () => {
+        mainWindow = null
     } )
 
     mainWindow.loadURL( 'file://' + __dirname + '/index.html' )
 
     mainWindow.webContents.openDevTools()
-
-    mainWindow.on( 'closed', function () {
-        mainWindow = null
-    } )
 } )
