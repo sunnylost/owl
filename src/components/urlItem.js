30fefd915c95e58156290729287904e521619d4b
sunnylost <sunnylost@gmail.com>
2016-05-27 10:26:54 +0800

    change list's layout implement

--- src/components/urlItem.js
+++ src/components/urlItem.js
@@ -4,13 +4,19 @@ class URLItem extends React.Component {
     render() {
         let props         = this.props,
             displayDetail = props.displayDetail,
-            { id, isActive, type, url } = props.data,
-            className     = 'url item ' + ( isActive ? 'active' : '' )
+            { id, isActive, domain, type, url, path, general } = props.data,
+            className     = 'url js-col item ' + ( isActive ? 'active' : '' )
 
-        return <li className={ className } onClick={ e => displayDetail( id ) }>
-            <i className={ 'c-icon c-icon-' + type } title={ type } data-type={ type }></i>
-            <span className="url val">{ url }</span>
-        </li>
+        return <div className={ className } onClick={ e => displayDetail( id ) }>
+            <div className="piece">
+                <i className={ 'c-icon c-icon-' + type } title={ type } data-type={ type }></i>
+            </div>
+            <div className="piece">{ path }</div>
+            <div className="piece">{ general.status }</div>
+            <div className="piece">{ general.method }</div>
+            <div className="piece">{ domain }</div>
+            <div className="piece">{ type }</div>
+        </div>
     }
 }
 
--- src/components/urlList.js
+++ src/components/urlList.js
@@ -2,13 +2,21 @@ import React from 'react'
 import URLItem from './urlItem'
 
 const URLList = ( { urls, displayDetail }={} ) => (
-    <ul className="list">
+    <div className="list js-flexible">
+        <div className="url col col-header js-col">
+            <div className="piece"></div>
+            <div className="piece">Name</div>
+            <div className="piece">Status</div>
+            <div className="piece">Method</div>
+            <div className="piece">Domain</div>
+            <div className="piece">Type</div>
+        </div>
         {
             urls.map( ( data ) => {
-                return <URLItem key={ data.id } displayDetail={ displayDetail } data={ data } />
+                return <URLItem key={ data.id } displayDetail={ displayDetail } data={ data }/>
             } )
         }
-    </ul>
+    </div>
 )
 
 export default URLList
