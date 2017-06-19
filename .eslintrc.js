897a2c0659bad064e27793651a9b8b5df7af0295
sunnylost <sunnylost@gmail.com>
2017-06-19 12:16:53 +0800

    update dep

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: "eslint:recommended",
+  plugins: ["react"],
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    indent: ["error", 4],
+    "object-curly-spacing": ["error", "always"],
+    "array-bracket-spacing": ["error", "always"],
+    "react/jsx-uses-vars": 2
+  }
+};
